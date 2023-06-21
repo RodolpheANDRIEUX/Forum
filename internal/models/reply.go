31567c298e6190be8699cb8b7c7f85956236cb3e
aflorian <florian.albora@ynov.com>
2023-06-21 16:07:05 +0200

    feat(): reply
    reply backend

--- internal/models/reply.go
+++ internal/models/reply.go
@@ -3,13 +3,12 @@ package models
 import "time"
 
 type Reply struct {
-	ReplyID    uint `gorm:"primarykey"`
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
-	PostID     uint
-	UserID     uint
-	User       User
-	Message    string
-	Picture    []byte
-	ReplyLikes []ReplyLike `gorm:"foreignKey:ReplyID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	ReplyID   uint `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	PostID    uint
+	UserID    uint
+	User      User
+	Message   string
+	Picture   []byte
 }
